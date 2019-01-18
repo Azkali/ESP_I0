2c7911a4a9a56837961fe87229befdee6b990219
Azkali <a.ffcc7@gmail.com>
2019-01-18 12:48:33 +0100

    fix: TFT_eSPImod added

---
+++ components/TFT_eSPI/component.mk
@@ -0,0 +1,17 @@
+COMPONENT_SRCIRS := \
+	. \
+	./Extensions/ \
+	./Fonts/ \
+	./TFT_Drivers/ \
+	./Tools/ \
+	./User_Setups/
+
+COMPONENT_ADD_INCLUDEDIRS := \
+	. \
+	./Extensions/ \
+	./Fonts/ \
+	./TFT_Drivers/ \
+	./Tools/ \
+	./User_Setups/ \
+	./include/
+
